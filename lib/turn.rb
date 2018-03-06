20b58bc5a151a88ff4c1f3487259b81c23a1c6cc
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-06 20:24:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,8 +40,8 @@ def turn(board)
   index = input_to_index(position)
   if valid_move?(board, index)
     move(board, index)
+    display_board(board)
   else
     turn(board)
   end
-  display_board(board)
 end
